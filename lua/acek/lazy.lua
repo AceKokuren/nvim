14367ea8e036b01837e234329389f29f54428bdd
acekokuren <acekokuren@gmail.com>
2026-01-07 14:41:20 +0000

    Moved jdtls to config directory

--- lua/acek/lazy.lua
+++ lua/acek/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     -- import your plugins
     { import = "acek.plugins" },
     { import = "acek.plugins.lsp" },
+    { import = "acek.plugins.java" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
