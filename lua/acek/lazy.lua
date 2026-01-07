c232e2f1652700d4e1a1d56c20d682c719dac5a3
acekokuren <acekokuren@gmail.com>
2026-01-07 16:33:00 +0000

    Removed Unnecessary Plugin Import

--- lua/acek/lazy.lua
+++ lua/acek/lazy.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
     -- import your plugins
     { import = "acek.plugins" },
     { import = "acek.plugins.lsp" },
-    { import = "acek.plugins.java" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
